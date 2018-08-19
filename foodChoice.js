f9eb098ec698632f30ec770046e17ec0d13b4e65
Michael Zalik <mike.zalik@gmail.com>
2018-08-18 23:33:54 -0700

    first commit

---
+++ foodChoice.js
@@ -0,0 +1,15 @@
+//Simple switch statement to help with choice of lunch
+//TODO: Make more intricate!
+
+var foodChoice;
+switch(Math.floor(Math.random() * 3)){
+case 0:
+text = "American";
+break;
+case 1:
+text = "Mexican";
+break;
+case 2:
+text = "Thai";
+break;
+}
